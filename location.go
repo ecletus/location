887b5a5840df8b55541feec333dac7fa2ab588ec
Moises P. Sena <moisespsena@gmail.com>
2019-03-15 18:06:25 -0300

    Rename project and dependencies

--- location.go
+++ location.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/aghape/admin"
-	"github.com/aghape/core"
-	"github.com/aghape/core/resource"
-	"github.com/aghape/core/utils"
+	"github.com/ecletus/admin"
+	"github.com/ecletus/core"
+	"github.com/ecletus/core/resource"
+	"github.com/ecletus/core/utils"
 )
 
 type Plugin struct {
